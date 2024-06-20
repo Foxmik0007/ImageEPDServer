ea39eb757951d96068c2bc807ea88bdda58fec6f
Mika Rafaralahy <71786918+Foxmik0007@users.noreply.github.com>
2024-06-20 11:01:45 -0500

    Update index.js

--- epd-server/src/api/index.js
+++ epd-server/src/api/index.js
@@ -17,7 +17,7 @@ const Jimp = require('jimp');
 */
 
 const app = express();
-const port = 3000;
+const port = 8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
